31eb05a450c54d5f789bce61618503af90b6e919
madnessxd <brotherhoodofnod201@hotmail.com>
2014-03-14 16:22:56 +0100

    Menu update (not done)

---
+++ AlmendeMotivator/src/alm/motiv/AlmendeMotivator/Messages.java
@@ -0,0 +1,24 @@
+package alm.motiv.AlmendeMotivator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Messages extends Activity{
+    Intent home;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messages);
+    }
+    @Override
+    public void onBackPressed() {
+        System.out.println("BACK");
+        home = new Intent(Messages.this, Main.class);
+        startActivity(home);
+        return;
+    }
+}
