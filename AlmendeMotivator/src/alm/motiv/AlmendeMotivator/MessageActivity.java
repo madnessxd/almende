aa62d103c785a2bfc3f73bf07cf7b4ac271ce57c
madnessxd <brotherhoodofnod201@hotmail.com>
2014-04-06 23:12:01 +0200

    Send message to server

--- AlmendeMotivator/src/alm/motiv/AlmendeMotivator/MessageActivity.java
+++ AlmendeMotivator/src/alm/motiv/AlmendeMotivator/MessageActivity.java
@@ -1,5 +1,6 @@
 package alm.motiv.AlmendeMotivator;
 
+import alm.motiv.AlmendeMotivator.adapters.MessagesAdapter;
 import alm.motiv.AlmendeMotivator.facebook.FacebookMainActivity;
 import alm.motiv.AlmendeMotivator.facebook.FacebookManager;
 import android.app.Activity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class MessageActivity extends Activity{
@@ -15,15 +17,25 @@ public class MessageActivity extends Activity{
     Intent k;
     private String[] mMenuOptions;
     private ListView mDrawerList;
+    //private ListView messageList;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messagesmenu);
+
         mMenuOptions = getResources().getStringArray(R.array.profile_array);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_menu, mMenuOptions));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //messageList = (ListView) findViewById(R.id.messagesGrid);
+        //messageList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_message, mMenuOptions));
+        //messageList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //GridView messageListView = (GridView) findViewById(R.id.messagesGrid);
+        //messageListView.setAdapter(new MessagesAdapter(this));
+
     }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
@@ -33,6 +45,12 @@ public class MessageActivity extends Activity{
         }
     }
 
+    public void createMessage(View v){
+        System.out.println("Er was eens een sout");
+        k = new Intent(MessageActivity.this, NewMessageActivity.class);
+        finish();
+        startActivity(k);
+    }
     public void selectItem(int pos){
         switch (pos){
             case 0:
@@ -62,4 +80,5 @@ public class MessageActivity extends Activity{
         startActivity(home);
         return;
     }
+
 }
---
+++ AlmendeMotivator/src/alm/motiv/AlmendeMotivator/NewMessageActivity.java
@@ -0,0 +1,110 @@
+package alm.motiv.AlmendeMotivator;
+
+import alm.motiv.AlmendeMotivator.facebook.FacebookMainActivity;
+import alm.motiv.AlmendeMotivator.facebook.FacebookManager;
+import alm.motiv.AlmendeMotivator.models.Message;
+import alm.motiv.AlmendeMotivator.models.User;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.*;
+import com.facebook.*;
+import com.facebook.model.GraphUser;
+import com.mongodb.*;
+
+public class NewMessageActivity extends Activity{
+    Intent home;
+    Intent k;
+    private String[] mMenuOptions;
+    private ListView mDrawerList;
+
+    private String message;
+    private String date;
+
+    private GraphUser user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        mMenuOptions = getResources().getStringArray(R.array.profile_array);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_menu, mMenuOptions));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    public void sendMessage(View v){
+        EditText mEdit = (EditText)findViewById(R.id.messageInput);
+        message = mEdit.getText().toString();
+        System.out.println(message);
+
+        Time now = new Time();
+        now.setToNow();
+        date = (now.year + "/" + now.month + "/" + now.monthDay + "-" + now.hour + ":" + now.minute);
+
+        DatabaseThread db = new DatabaseThread();
+        db.execute();
+        Toast.makeText(getApplicationContext(), "Message Send!", Toast.LENGTH_LONG).show();
+    }
+
+    class DatabaseThread extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... args) {
+            // To connect to mongodb server
+            MongoClient client = Database.getInstance();
+            DB db = client.getDB(Database.uri.getDatabase());
+
+            //get collection and attach class to it
+            DBCollection userCollection = db.getCollection("messages");
+            userCollection.setObjectClass(Message.class);
+
+            //TODO Add Challengee from appFriendslist
+            //System.out.println(user.getId());
+            Message challenge = new Message("a", "b", "Test Message", message, date, "Normal message", "0");
+            userCollection.insert(challenge, WriteConcern.ACKNOWLEDGED);
+            return null;
+        }
+    }
+
+    public void selectItem(int pos){
+        switch (pos){
+            case 0:
+                k = new Intent(NewMessageActivity.this, ProfileActivity.class);
+                break;
+            case 1:
+                k = new Intent(NewMessageActivity.this, MessageActivity.class);
+                break;
+            case 2:
+                k = new Intent(NewMessageActivity.this, ChallengesMenuActivity.class);
+                break;
+            case 3:
+                k = new Intent(NewMessageActivity.this, FriendActivity.class);
+                break;
+            case 4:
+                FacebookManager.logout();
+                k = new Intent(NewMessageActivity.this, FacebookMainActivity.class);
+                break;
+        }
+        finish();
+        startActivity(k);
+    }
+    @Override
+    public void onBackPressed() {
+        finish();
+        home = new Intent(NewMessageActivity.this, MainMenuActivity.class);
+        startActivity(home);
+        return;
+    }
+
+}
